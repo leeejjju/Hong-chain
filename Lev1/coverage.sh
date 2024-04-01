d262696a7f59eedfd7d1ac396027ad3c9b0356ba
leeejjju <leeejjju@gmail.com>
2024-04-01 17:44:12 +0900

    modify coverage.sh

--- Lev1/coverage.sh
+++ Lev1/coverage.sh
@@ -5,14 +5,7 @@ mkdir -p cov/crashes
 mkdir -p cov/queue
 
 # Process crashes
-# for file in outputs/default/crashes/*; do
-#     if [ -f "$file" ]; then
-#         ./btree < "$file"
-#         gcov -bcif "$file"
-#         mv *.gcov "cov/crashes/$(basename "$file").gcov"
-#         rm -f *.gcda
-#     fi
-# done
+# need human's touch...
 
 # Process queue
 for file in outputs/default/queue/*; do
@@ -20,6 +13,5 @@ for file in outputs/default/queue/*; do
         ./btree < "$file"
         gcov -bcif *.gcda
         mv *.gcov "cov/queue/$(basename "$file").gcov"
-        rm -f *.gcda
     fi
 done
